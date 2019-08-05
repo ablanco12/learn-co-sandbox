0acd96f7708b450a8acf8473fe3b7214ab84bd16
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-08-05 14:03:16 +0000

    Automatically backed up by Learn

---
+++ Hashes.rb
@@ -0,0 +1,4 @@
+key = :breakfast
+
+meals = {:breakfast => "cereal", :lunch => "peanut butter and jelly sandwich", :dinner => "mushroom risotto"}
+meals[key]
---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+# From the Three Dog Night song: "Joy to the World (Jermiah was a bullfrog)"
+puts "Joy to the world"
+puts "All boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
